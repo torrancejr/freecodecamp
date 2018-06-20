f14833965f023d9632ab682be061013b76432f33
Ryan Torrance <torrance.jr@gmail.com>
2018-06-20 11:03:41 -0400

    styling

---
+++ styling.js
@@ -0,0 +1,10 @@
+
+const styles ={color: 'purple', fontSize: '40px', border: '2px solid purple'};
+
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{styles}}>Style Me!</div>
+    );
+  }
+};
